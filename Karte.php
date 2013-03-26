dde7d5f17cc24f61d8a00addba85beeb4f55bb2b
Markus <mkonkol@arcor.de>
2013-03-26 17:02:51 +0100

    Undo

--- Karte.php
+++ Karte.php
@@ -6,7 +6,6 @@ linkages to the help-site, the table and the diagram.  -->
 <head>
   <meta http-equiv="content-type" content="text/html; charset=utf-8">
   <title>Air Quality Egg Muenster</title>
-  <link rel="shortcut icon" href="bilder/egg1.png" type="image/x-icon" />
   <meta name="description" content="Luftqualitätsdaten Münster">
   <meta name="keywords" content="Air Quality Egg">
   <meta name="viewport" content="target-densitydpi=480px, width=1000px, user-scalable=yes">

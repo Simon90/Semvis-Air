4e61bc7660d6ed6bbb4d46885ab92d3a31b7ea80
StephanieWalter <stephiewalter89@googlemail.com>
2013-04-12 01:13:27 +0200

    Rohdatentabelle mit radio buttons

--- createTableRawData.php
+++ createTableRawData.php
@@ -3,51 +3,56 @@
 //Create connection
 $dbconn = pg_connect("host=giv-geosoft2c.uni-muenster.de port=5432 dbname=CosmDaten user=geosoft2 password=DZLwwxbW");
 
-$dateBegin = $_GET["dateBegin"];
-$monthBegin = $_GET["monthBegin"];
-$yearBegin = $_GET["yearBegin"];
+$date = $_GET["date"];
+$month = $_GET["month"];
+$year = $_GET["year"];
 
-$timeBegin = $yearBegin . '-' . $monthBegin . '-' . $dateBegin;
+$timeBegin = $year . '-' . $month . '-' . $date;
 
-$dateEnd = $_GET["dateEnd"];
-$monthEnd = $_GET["monthEnd"];
-$yearEnd = $_GET["yearEnd"];
-
-$timeEnd = $yearEnd . '-' . $monthEnd . '-' . $dateEnd;
-
+$timeEnd = $year . '-' . $month . '-' . $date . ' 23:59:59';
 $sensorId1 = $_GET["sensorId1"];
 
-$sql_select = "SELECT * FROM \"MeasuredData\" WHERE \"sensorId\" = '$sensorId1' AND date BETWEEN '$timeBegin' AND '$timeEnd'";
+$sql_select = "SELECT * FROM \"MeasuredData\" WHERE \"sensorId\" = '$sensorId1' AND date BETWEEN '$timeBegin' AND '$timeEnd' ORDER BY date";
 
 $result = pg_query($dbconn, $sql_select);
 
-echo 'Rohdaten des Air Quality Eggs ' . $sensorId1;
+echo "name1 = " . $sensorId1 . ";";
 
-echo "<table width=250 border=1>";
-echo "<tr>";
-echo "<th>" ."Datum"."</th>";
-echo "<th>" ."Temperatur"."</th>";
-echo "<th>" ."Ozon"."</th>";
-echo "<th>" ."Kohlenstoffmonoxid"."</th>";
-echo "<th>" ."Stickstoffdioxid"."</th>";
-echo "<th>" ."Luftfeuchtigkeit"."</th>";
-echo "</tr>";
+echo "table[0] = '<table width=250 border=1><tr><th>Datum</th><th>Temperatur</th><th>Ozon</th><th>Kohlenstoffmonoxid</th><th>Stickstoffdioxid</th><th>Luftfeuchtigkeit</th></tr>';";
+
+
+echo 'table[1] = "';
 while($row = pg_fetch_array($result))
     {
 	echo "<tr>";
     echo "<td>" .$row["date"] ."</td>";	
-    echo "<td>" .$row["temperature"] ."</td>";
-	echo "<td>" .$row["ozon"] ."</td>";
-	echo "<td>" .$row["co"] ."</td>";
-	echo "<td>" .$row["no2"] ."</td>";
-	echo "<td>" .$row["humidity"] ."</td>";
+	if ($row["temperature_validated"] == 't'){
+		echo "<td><span style='color:red'>" .$row["temperature"] ."</span></td>";
+		}
+		else {echo "<td>" .$row["temperature"] ."</td>";}
+	if ($row["ozon_validated"] == 't'){
+		echo "<td><span style='color:red'>" .$row["ozon"] ."</span></td>";
+		}
+		else {echo "<td>" .$row["ozon"] ."</td>";}
+	if ($row["co_validated"] == 't'){
+		echo "<td><span style='color:red'>" .$row["co"] ."</span></td>";
+		}
+		else {echo "<td>" .$row["co"] ."</td>";}
+	if ($row["no2_validated"] == 't'){
+		echo "<td><span style='color:red'>" .$row["no2"] ."</span></td>";
+		}
+		else {echo "<td>" .$row["no2"] ."</td>";}
+	if ($row["humidity_validated"] == 't'){
+		echo "<td><span style='color:red'>" .$row["humidity"] ."</span></td>";
+		}
+		else {echo "<td>" .$row["humidity"] ."</td>";}
     echo "</tr>";
-	} 
-echo "</table>";
+	}	
+echo '</table>";';
 	
 pg_close($dbconn);
 
 //url zum testen:
-//http://localhost/Semvis-Air/createTableRawData.php?sensorId1=75842&dateBegin=19&monthBegin=3&yearBegin=2013&dateEnd=20&monthEnd=3&yearEnd=2013
+//http://localhost/Semvis-Air/createTableRawData.php?sensorId1=75842&dateBegin=09&monthBegin=4&yearBegin=2013
 
 ?>
